f73905e74b09f9b98e24bc21b7e39514a1a85707
Sacha Yunusic <syunusic@bitbucket.org>
2026-06-30 20:59:22 -0400

    add moída/moídas to TRAILING_MODIFIERS; reparse 3992 ingredients

--- backend/scripts/fix_ingredient_names.py
+++ backend/scripts/fix_ingredient_names.py
@@ -18,7 +18,7 @@ UNIT_WORDS = re.compile(
     re.IGNORECASE,
 )
 TRAILING_MODIFIERS = re.compile(
-    r"\s+(rallada|rallado|ralladas|rallados|tostado|tostada|tostados|tostadas|fresco|fresca|frescos|frescas|molida|molidos|molidas|triturado|triturada|triturados|trituradas|picado|picada|picados|picadas|congelado|congelada|congelados|congeladas|desalado|desalada|desalados|desaladas|ahumado|ahumada|ahumados|ahumadas|remojado|remojada|remojados|remojadas|deshuesado|deshuesada|en\s+salazón)$",
+    r"\s+(rallada|rallado|ralladas|rallados|tostado|tostada|tostados|tostadas|fresco|fresca|frescos|frescas|molida|molidos|molidas|moída|moídas|triturado|triturada|triturados|trituradas|picado|picada|picados|picadas|congelado|congelada|congelados|congeladas|desalado|desalada|desalados|desaladas|ahumado|ahumada|ahumados|ahumadas|remojado|remojada|remojados|remojadas|deshuesado|deshuesada|en\s+salazón)$",
     re.IGNORECASE,
 )
 
